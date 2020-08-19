42cb7f3767bdee55abccb98d668902a280067328
arellanog <arellanog@cent.gov.ar>
2020-08-19 12:52:26 -0300

    Primera versión de la aplicación sin base de datos

--- src/main/java/io/github/aretche/learningspringboot/LearningSpringBootApplication.java
+++ src/main/java/io/github/aretche/learningspringboot/LearningSpringBootApplication.java
@@ -3,11 +3,19 @@ package io.github.aretche.learningspringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
+
 @SpringBootApplication
 public class LearningSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LearningSpringBootApplication.class, args);
+    }
+
+    @Bean
+    HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
     }
 
 }
